f85be3c50f31e5d6ee6a528e857359b88de7afd5
Jerry Hobby <Jerry@JerryHobby.com>
2023-09-06 14:36:14 -0500

    init

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,68 +1,20 @@
-import 'package:clima/models/weather_data.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/services/weather.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
-  final double? longitude;
-  final double? latitude;
-
-  LoadingScreen({Key? key, this.longitude, this.latitude}) : super(key: key);
-
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  late Weather weather;
-  late WeatherData weatherData;
-  late BuildContext context;
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.longitude != null && widget.latitude != null) {
-      weather = Weather(
-          longitude: widget.longitude,
-          latitude: widget.latitude,
-          notify: () => callback());
-    } else {
-      weather = Weather(notify: () => callback());
-    }
-  }
-
-  callback() {
-    weatherData = WeatherData(
-        weatherData: weather.weatherData, cityState: weather.cityState);
-
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen(
-        weatherData: weatherData,
-        cityState: weather.cityState,
-      );
-    }));
-  }
-
   @override
   Widget build(BuildContext context) {
-    // once weather finishes fetching,
-    // it will redirect to the next page.
-    this.context = context;
-
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            SpinKitDoubleBounce(
-              color: Color.fromRGBO(50, 50, 255, 1),
-              duration: Duration(milliseconds: 1500),
-              size: 150.0,
-            ),
-          ],
+        child: TextButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: Text('Get Location'),
         ),
       ),
     );
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,148 +1,22 @@
 import 'package:flutter/material.dart';
-import 'loading_screen.dart';
+import 'package:clima/utilities/constants.dart';
 
 class LocationScreen extends StatefulWidget {
-  final weatherData;
-  final cityState;
-
-  const LocationScreen({
-    Key? key,
-    this.weatherData,
-    this.cityState,
-  }) : super(key: key);
-
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
-  TextStyle textStyleSmall = TextStyle(
-    fontSize: 20,
-  );
-  TextStyle textStyleNormal = TextStyle(
-    fontSize: 26,
-  );
-  TextStyle textStyleLarge = TextStyle(
-    fontSize: 70,
-  );
-
-  get weatherData {
-    return widget.weatherData;
-  }
-
-  get weatherMessage {
-    return weatherData.weatherTempImperial == null
-        ? null
-        : getMessage(weatherData.weatherTempImperial!);
-  }
-
-  get weatherIcon {
-    return weatherData.weatherConditionCode == null
-        ? null
-        : getWeatherIcon(weatherData.weatherConditionCode!);
-  }
-
-  String getWeatherIcon(int condition) {
-    if (condition < 300) {
-      return '🌩';
-    } else if (condition < 400) {
-      return '🌧';
-    } else if (condition < 600) {
-      return '☔️';
-    } else if (condition < 700) {
-      return '☃️';
-    } else if (condition < 800) {
-      return '🌫';
-    } else if (condition == 800) {
-      return '☀️';
-    } else if (condition <= 804) {
-      return '☁️';
-    } else {
-      return '🤷‍';
-    }
-  }
-
-  String getMessage(double temp) {
-    String tag = '';
-
-    // if (cityState != null) {
-    //   tag = 'in $cityState';
-    // }
-    if (temp > 90) {
-      return 'It\'s 🍦 time $tag';
-    } else if (temp > 75) {
-      return 'Time for 🩳 and 👕 $tag';
-    } else if (temp < 60) {
-      return 'You\'ll need 🧣 and 🧤 $tag';
-    } else {
-      return 'Bring a 🧥 just in case $tag';
-    }
-  }
-
-  Widget moonPhase() {
-    double moonPhase = weatherData.moonPhase ?? 0.0;
-    String moonPic;
-
-    if (moonPhase < 0.1) {
-      moonPic = 'images/0.000 moon.png';
-    } else if (moonPhase < 0.2) {
-      moonPic = 'images/0.125 moon.png';
-    } else if (moonPhase < 0.3) {
-      moonPic = 'images/0.250 moon.png';
-    } else if (moonPhase < 0.6) {
-      moonPic = 'images/0.500 moon.png';
-    } else if (moonPhase < 0.8) {
-      moonPic = 'images/0.750 moon.png';
-    } else if (moonPhase < 0.9) {
-      moonPic = 'images/0.875 moon.png';
-    } else {
-      moonPic = 'images/1.000 moon.png';
-    }
-
-    Widget moon = Image(
-      image: AssetImage('$moonPic'),
-      height: 70,
-    );
-
-    if (weatherData.moonPhase == null) {
-      return Text('');
-    } else {
-      return moon;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-
-    ButtonStyle buttonStyle = ButtonStyle(
-      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-        RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-      ),
-    padding: MaterialStateProperty.all<EdgeInsets>( EdgeInsets.all(5.0),
-    ));
-
-    TextStyle buttonTextStyle = TextStyle(
-      fontSize: 22,
-    );
-
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('images/location_background.jpg'),
             fit: BoxFit.cover,
-
-            colorFilter: ColorFilter.matrix(<double>[
-              0.2, 0.1, 0.2, 0.0, 0.0, // matrix
-              0.2, 0.1, 0.1, 0.1, 0.0,
-              0.3, 0.4, 0.2, 0.3, 0.0,
-              1, 1, 1, 1, 0,
-            ]),
-
-            // colorFilter: ColorFilter.mode(
-            //     Colors.white.withOpacity(0.99), BlendMode.dstATop),
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
           ),
         ),
         constraints: BoxConstraints.expand(),
@@ -155,147 +29,48 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   ElevatedButton(
-                    style: buttonStyle,
-                    onPressed: () {
-                      changeLocation(city: "home", context: context);
-                    },
-                    child: Text(
-                      "Home",
-                      style: buttonTextStyle,
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.near_me,
+                      size: 50.0,
                     ),
                   ),
                   ElevatedButton(
-                    style: buttonStyle,
-                    onPressed: () {
-                      changeLocation(city: "tokyo", context: context);
-                    },
-                    child: Text(
-                      "Tokyo",
-                      style: buttonTextStyle,
-                    ),
-                  ),
-                  ElevatedButton(
-                    style: buttonStyle,
-                    onPressed: () {
-                      changeLocation(city: "london", context: context);
-                    },
-                    child: Text(
-                      "London",
-                      style: buttonTextStyle,
-                    ),
-                  ),
-                  ElevatedButton(
-                    style: buttonStyle,
-                    onPressed: () {
-                      changeLocation(city: "san fernando", context: context);
-                    },
-                    child: Text(
-                      "San Fernando",
-                      style: buttonTextStyle,
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.location_city,
+                      size: 50.0,
                     ),
                   ),
                 ],
               ),
-              Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(30),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(0, 0, 100, 0.70),
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Colors.indigo[400]!,
-                    width: 2,
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          weatherIcon ?? "",
-                          style: textStyleLarge,
-                        ),
-                        moonPhase(),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
+              Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '32°',
+                      style: kTempTextStyle,
                     ),
                     Text(
-                      weatherData.cityState ?? "",
-                      style: textStyleNormal,
-                    ),
-                    Text(
-                      weatherData.weatherDateTimeString ?? "",
-                      style: textStyleSmall,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(weatherData.weatherTempImperialString ?? "",
-                        style: textStyleLarge),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Divider(
-                      color: Colors.indigo[400]!,
-                      thickness: 2,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      weatherMessage ?? "",
-                      style: textStyleNormal,
+                      '☀️',
+                      style: kConditionTextStyle,
                     ),
                   ],
                 ),
               ),
-              SizedBox.fromSize(
-                size: Size(0, 70),
+              Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  "It's 🍦 time in San Francisco!",
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
               ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  changeLocation({city, context}) {
-    double? lat = null;
-    double? lon = null;
-
-    if (city != null) {
-      switch (city) {
-        case 'london':
-          lat = 51.481127;
-          lon = -0.452956;
-          break;
-        case 'tokyo':
-          lat = 35.680060;
-          lon = 139.761564;
-          break;
-        case 'san fernando':
-          lat = 15.063690;
-          lon = 120.650761;
-          break;
-      }
-    }
-
-    if (lat != null && lon != null) {
-      Navigator.push(context, MaterialPageRoute(builder: (context) {
-        return LoadingScreen(
-          latitude: lat,
-          longitude: lon,
-        );
-      }));
-    } else {
-      Navigator.push(context, MaterialPageRoute(builder: (context) {
-        return LoadingScreen();
-      }));
-    }
   }
 }
